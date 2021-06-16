8f5c61c524519260d675a381fef62c5132be3fa5
Luis Fernando <luiscaute4@gmail.com>
2021-06-15 22:27:44 -0500

    Primer video: creacion de proyecto y primeras pruebas

---
+++ src/main/java/com/ejemplo/javatests/util/StringUtil.java
@@ -0,0 +1,7 @@
+package com.ejemplo.javatests.util;
+
+public class StringUtil {
+    public String repeat(String str, int times){
+        return null;
+    }
+}
---
+++ src/test/java/com/ejemplo/javatests/util/StringUtilTest.java
@@ -0,0 +1,5 @@
+package com.ejemplo.javatests.util;
+
+class StringUtilTest {
+
+}
